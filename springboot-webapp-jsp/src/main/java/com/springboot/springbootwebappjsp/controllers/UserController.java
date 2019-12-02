283475e3d4080d8caaa835bb3b76e44fd1ec2518
ttaylor22 <tevtaylor21@gmail.com>
2019-12-02 16:47:03 -0500

    jsp to thymeleaf conversion and added spring security

--- springboot-webapp-jsp/src/main/java/com/springboot/springbootwebappjsp/controllers/UserController.java
+++ springboot-webapp-jsp/src/main/java/com/springboot/springbootwebappjsp/controllers/UserController.java
@@ -5,18 +5,27 @@ package com.springboot.springbootwebappjsp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 import com.springboot.springbootwebappjsp.models.User;
 import com.springboot.springbootwebappjsp.repositories.UserRepository;
 
+
 @Controller
+
 public class UserController {
 	@Autowired
-	UserRepository userRepo;
+    UserRepository userRepo;
+	
+//	@RequestMapping(value= {"/**"}, method=RequestMethod.GET)
+//	public ModelAndView base() {
+//		return new ModelAndView ("redirect:/login");
+//	}
 	
 	@GetMapping("/users")
 	public String getUsers(Model model) {
